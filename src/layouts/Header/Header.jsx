b445bcf8e0f3346bd032fc9d09b2de941742df9c
Nikita Karpov <jacquesjaws@yandex.ru>
2025-06-12 11:56:47 +0400

    Initialization project

---
+++ src/layouts/Header/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className="header__promo">
+        <div className="header__promo-inner">
+          <div className="header__promo-link">Admission is Open, Grab your seat now</div>
+        </div>
+      </div>
+      <div className="header__body">
+        <div className="header__body-inner"></div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/pages/about.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'About',
+}
+
+export default function () {
+  return (
+    <>
+      <h1>About</h1>
+    </>
+  )
+}
